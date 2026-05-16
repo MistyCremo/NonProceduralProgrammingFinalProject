31c14149931db8ea2652ea4eba6a8eb124f29371
Misty Cremo <mistycremo@gmail.com>
2026-05-16 14:40:22 +0200

    Added a test file and a predicate to read it to a string

--- project.pl
+++ project.pl
@@ -4,4 +4,8 @@ heads([[X|_]|L], [X|M]) :- heads(L,M).
 tails([],[]).
 tails([[_|Y]|L], [Y|M]) :- tails(L,M).
 
-transpose([[]|L],[[]]).
+transpose([[]|_],[[]]).
+
+args(X) :- current_prolog_flag(argv,X).
+
+file(Y) :- args(XS), [X|_] = XS, atom_string(X,FS), read_file_to_string(FS,Y,[]).
