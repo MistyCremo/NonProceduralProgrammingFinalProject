adc91a9d74a86f54a96593cfa49f24dd26e0ea89
Misty Cremo <mistycremo@gmail.com>
2026-05-22 17:20:28 +0200

    Adds working transpose function for rectangular matrices

--- project.pl
+++ project.pl
@@ -1,11 +1,9 @@
-heads([],[]).
-heads([[X|_]|L], [X|M]) :- heads(L,M).
-
-tails([],[]).
-tails([[_|Y]|L], [Y|M]) :- tails(L,M).
-
-transpose([[]|_],[[]]).
-
 args(X) :- current_prolog_flag(argv,X).
 
 file(Y) :- args(XS), [X|_] = XS, atom_string(X,FS), read_file_to_string(FS,Y,[]).
+
+heads_and_tails([],[],[]).
+heads_and_tails([[H|T]|L],[H|Heads],[T|Tails]) :- heads_and_tails(L,Heads,Tails).
+
+transpose(L,[]) :- maplist( =([]), L).
+transpose(L,[H|M]) :- heads_and_tails(L,H,T), transpose(T,M).
