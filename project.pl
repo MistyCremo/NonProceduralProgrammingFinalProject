6a9bfdd6a38deec576bef1d34cebc5da1f846c10
Misty Cremo <mistycremo@gmail.com>
2026-06-05 16:26:03 +0200

    Honestly, I did a lot all at once, and I know that's pretty bad practice, but it's too late now

--- project.pl
+++ project.pl
@@ -12,12 +12,25 @@ myTranspose(L,[H|M]) :- heads_and_tails(L,H,T), myTranspose(T,M).
 
 
 edgegrid([X|G],E) :- X1 + 1 #= X, length([X|G],L), edgegrid1([X|G],X,X1,E,L).
-edgegrid1([X],X,0,[1],1).
+edgegrid1([X],X,0,[1],L) :- L #>= 0.
 edgegrid1([X|G],X,N,[0|E],L) :- L #>= X, X #> 0,N #> 0, X #>= N, N1 + 1 #= N, edgegrid1(G,X,N1,E,L).
 edgegrid1([X,Y|G],X,0,[1|E], L) :- L #>= X, X #> 0, Y #> 0, Y1 + 1 #= Y, edgegrid1([Y|G],Y,Y1,E,L).
 
 hasEdgegrid(L) :- edgegrid(L,_).
 
-isSquare(LL) :- maplist(same_length(LL), LL).
+isRect([X|LL]) :- maplist(same_length(X), LL).
 
-squaregrid(LL) :- isSquare(LL), maplist(hasEdgegrid,LL), maplist(hasEdgegrid, TL), myTranspose(LL,TL).
+squaregrid(LL) :- isRect(LL), myTranspose(LL,TL), maplist(hasEdgegrid,LL), maplist(hasEdgegrid, TL).
+
+matches(X,X).
+matches(_,0).
+matches(0,_).
+
+allMatches(L,M) :- maplist(matches, L, M).
+
+allAllMatches(LL,MM) :- maplist(allMatches, LL, MM).
+
+split_string1(Sep,Pad,Sub,String) :- split_string(String,Sep,Pad,Sub).
+
+stringToLL(S, LL) :- split_string(S, "\n", "\n", L), maplist(split_string1(" ", " "), LL, L).
+
